0f12e01dd4b122c535b777f74600541029b2a4a3
Akshay <akshay.kumar3851@gmail.com>
2019-11-16 11:52:57 +0530

    First Commit

---
+++ src/main/java/com/akshay/springbothelloworld/SpringBotHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.springbothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBotHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBotHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshay/springbothelloworld/SpringBotHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.springbothelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBotHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
