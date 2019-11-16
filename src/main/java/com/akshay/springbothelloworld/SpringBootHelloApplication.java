d28b368d0dbbcc1c077a1ed0f9e398ba1ef592f1
Akshay <akshay.kumar3851@gmail.com>
2019-11-16 20:29:06 +0530

    servlet with @Bean

--- src/main/java/com/akshay/springbothelloworld/SpringBootHelloApplication.java
+++ src/main/java/com/akshay/springbothelloworld/SpringBootHelloApplication.java
@@ -1,15 +1,45 @@
 package com.akshay.springbothelloworld;
 
+import com.akshay.springbothelloworld.servletclasses.HelloFilter;
+import com.akshay.springbothelloworld.servletclasses.HelloListener;
+import com.akshay.springbothelloworld.servletclasses.HelloServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@ServletComponentScan
+// @ServletComponentScan
 public class SpringBootHelloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloApplication.class, args);
 	}
+
+	// Register Servlet
+	@Bean
+    public ServletRegistrationBean getServletRegistrationBean() {
+	    ServletRegistrationBean servletBean = new ServletRegistrationBean(new HelloServlet());
+	    servletBean.addUrlMappings("/helloServlet");
+	    return servletBean;
+    }
+
+    // Register Filter
+    @Bean
+    public FilterRegistrationBean getFilterRegistrationBean() {
+	    FilterRegistrationBean filterBean = new FilterRegistrationBean(new HelloFilter());
+	    // add url Patterns
+	    filterBean.addUrlPatterns("/helloServlet");
+	    return filterBean;
+    }
+
+    // Register Listener
+    public ServletListenerRegistrationBean getServletListenerRegistrationBean() {
+	    ServletListenerRegistrationBean listenerBean = new ServletListenerRegistrationBean(new HelloListener());
+	    return listenerBean;
+    }
 
 }
